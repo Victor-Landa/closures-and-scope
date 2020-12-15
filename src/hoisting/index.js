6aa361bc089040919c1a4b1f657a06538c0acbb2
Victor Landa <victorlanda.dev@gmail.com>
2020-12-14 23:16:47 -0600

    case 2 fucntion hoisting

--- src/hoisting/index.js
+++ src/hoisting/index.js
@@ -19,3 +19,39 @@ var nombre = 'Victor';
 var nombre;
 console.log(nombre);
 nombre = 'Victor';
+
+// Caso 2: Function antes del hoisting
+function filtrarProductos(productos) {
+  var productosConOferta = [];
+  for(var i = 0; i < productos.length; i++) {
+    var producto = productos[i];
+    if(producto.tieneDescuento) {
+      var descuento = producto.descuento;
+      if(descuento > 25) {
+        productosConOferta.push(producto);
+      }
+    }
+  }
+  return productosConOferta;
+}
+
+// Caso 2: Function después del hoisting
+function filtrarProductos(productos) {
+  var productosConOferta;
+  var i;
+  var producto;
+  var descuento;
+
+  productosConOferta = [];
+  for(i = 0; i < productos.length; i++) {
+    producto = productos[i];
+    if(producto.tieneDescuento) {
+      descuento = producto.descuento;
+      if(descuento > 25) {
+        productosConOferta.push(producto);
+      }
+    }
+  }
+
+  return productosConOferta;
+}
